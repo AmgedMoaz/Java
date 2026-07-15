70a04ac2c08257858bd4c48cd3a46facd9ab9750
Amged Moaz <amgedmoaz7@gmail.com>
2026-07-15 21:01:28 +0300

    Add Task 3

---
+++ Begining/1st Project/src/Task_3.java
@@ -0,0 +1,18 @@
+// Write a program to get two no from user and print their sum
+
+import java.util.Scanner;
+
+public class Task_3 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number 1 : ");
+        int num1 = input.nextInt();
+        System.out.print("Enter number 2 : ");
+        int num2 = input.nextInt();
+        int sum = num1 + num2;
+        System.out.printf("Sum : %d%n", sum);
+        input.close();
+
+    }
+}
