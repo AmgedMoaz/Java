142c792477740352abb95349dbbe48dc3c792c7a
Amged Moaz <amgedmoaz7@gmail.com>
2026-07-15 19:49:02 +0300

    Add Task 1 by java

---
+++ Begining/1st Project/src/Task_1.java
@@ -0,0 +1,10 @@
+// write a program to print some data
+public class Task_1 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Java");
+        System.out.println("My name is Amged Moaz");
+        System.out.println("I'm learning Java");
+
+    }
+}
