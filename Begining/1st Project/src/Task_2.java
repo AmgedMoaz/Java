b077d2e90efe0ac25c2bf39487a6654020e47a97
Amged Moaz <amgedmoaz7@gmail.com>
2026-07-15 20:32:55 +0300

    Add Task 2 by java

---
+++ Begining/1st Project/src/Task_2.java
@@ -0,0 +1,14 @@
+// Write a program to print variable's data
+public class Task_2 {
+    public static void main(String[] args) {
+        String name = "Amged Moaz";
+        int age = 21;
+        double height = 188.0;
+
+        // Show the output
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+
+    }
+}
