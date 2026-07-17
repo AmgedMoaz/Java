8ae5a6d28bd35279c7c562334077fd690bb2dbd9
Amged Moaz <amgedmoaz7@gmail.com>
2026-07-17 14:37:37 +0300

    Modify Task 4 by java

--- Begining/1st Project/src/Task_4.java
+++ Begining/1st Project/src/Task_4.java
@@ -4,17 +4,20 @@ import java.util.Scanner;
 
 public class Task_4 {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a number : ");
         int number = input.nextInt();
 
-        // Check Positive or negative
+        // Check the sign of the number
         if (number > 0) {
             System.out.print("Positive");
         } else if (number < 0) {
             System.out.print("Negative");
         } else {
-            System.out.print("zero");
+            System.out.print("Zero");
         }
+        input.close();
+
     }
 }
