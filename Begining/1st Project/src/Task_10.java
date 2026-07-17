9650d8ea96ffe420598222f9c9c6e1f3fcf26b2b
Amged Moaz <amgedmoaz7@gmail.com>
2026-07-17 17:10:14 +0300

    Add Task 10 by java

--- Begining/1st Project/src/Task_10.java
+++ Begining/1st Project/src/Task_10.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+// Write a program to check the number divide by 3 or 5
+
+import java.util.Scanner;
 
 public class Task_10 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int number = input.nextInt();
+
+        for (int i = 1; i <= number; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                System.out.println("FizzBuzz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(i);
+            }
+        }
+        input.close();
+        
+    }
 }
