8d8349b7aad9e20757a8bc8ba81b6f12246212d2
Amged Moaz <amgedmoaz7@gmail.com>
2026-07-17 16:14:18 +0300

    Add Task 8 by java

--- Begining/1st Project/src/Task_8.java
+++ Begining/1st Project/src/Task_8.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+// Write a program to print Multiplication Table
+
+import java.util.Scanner;
 
 public class Task_8 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int number = input.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            int multiplication = number * i;
+            System.out.printf("%d * %d = %d%n", number, i, multiplication);
+        }
+        input.close();
+
+    }
 }
