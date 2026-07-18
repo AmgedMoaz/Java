2517ff3c1a2ef85997a4b90d4f864c769f807311
Amged Moaz <amgedmoaz7@gmail.com>
2026-07-18 15:45:08 +0300

    Add Task 11 by java

---
+++ Begining/1st Project/src/Task_11.java
@@ -0,0 +1,12 @@
+// Write a method to print a Hello, Methods.
+public class Task_11 {
+    public static void main(String[] args) {
+
+        display();
+
+    }
+
+    public static void display() {
+        System.out.println("Hello, Methods.");
+    }
+}
