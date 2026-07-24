ae7afb6c82e73d460a531b55f335251ee8f333e2
Amged Moaz <amgedmoaz7@gmail.com>
2026-07-24 21:36:53 +0300

    Implement rectangle class at main class

--- Begining/OOP/src/Main.java
+++ Begining/OOP/src/Main.java
@@ -6,5 +6,20 @@ public class Main {
   static Scanner input = new Scanner(System.in);
   public static void main(String[] args) {
 
+      Rectangle ob = new Rectangle();
+      System.out.print("Enter double width : ");
+      double Width = input.nextDouble();
+      ob.set_width(Width);
+      System.out.print("Enter double length : ");
+      double length = input.nextDouble();
+      ob.set_length(length);
+
+      double rectWidth =  ob.get_width();
+      double rectLength = ob.get_length();
+      System.out.println("Rectangle Width is : " + rectWidth);
+      System.out.println("Rectangle Length is : " + rectLength);
+
+      double rectArea = ob.Area();
+      System.out.println("Rectangle Area is : " + rectArea);
   }
 }
