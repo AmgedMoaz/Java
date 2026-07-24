70e5f8c8ad2a249d6d861700fcd0127f85e20a8b
Amged Moaz <amgedmoaz7@gmail.com>
2026-07-24 21:18:31 +0300

    Add rectangle class

---
+++ Begining/OOP/src/Rectangle.java
@@ -0,0 +1,20 @@
+public class Rectangle {
+    private double width;
+    private double length;
+
+    public void set_width(double w) {
+        width = w;
+    }
+    public void set_length(double l) {
+        length = l;
+    }
+    public double get_width() {
+        return width;
+    }
+    public double get_length() {
+        return length;
+    }
+    public double Area() {
+        return (width*length);
+    }
+}
