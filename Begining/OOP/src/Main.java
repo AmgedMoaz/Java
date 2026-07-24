c194756be63d49aa95cdd177017e2c0009ae2a98
Amged Moaz <amgedmoaz7@gmail.com>
2026-07-24 21:03:27 +0300

    Add main class

---
+++ Begining/OOP/src/Main.java
@@ -0,0 +1,10 @@
+ // Write a main class for many classes
+
+ import java.util.Scanner;
+
+public class Main {
+  static Scanner input = new Scanner(System.in);
+  public static void main(String[] args) {
+
+  }
+}
