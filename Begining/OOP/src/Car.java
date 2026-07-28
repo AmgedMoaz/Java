992b961103ea135b0da9f4d65f9a439b31599e6f
Amged Moaz <amgedmoaz7@gmail.com>
2026-07-28 18:24:58 +0300

    Fill car class

--- Begining/OOP/src/Car.java
+++ Begining/OOP/src/Car.java
@@ -1,5 +1,39 @@
- // Write a simple class about car and use constructor and destructor
+ // Write a simple class about car and use constructor
 
 public class Car {
+
+    // Fields
+    private int model;
+    private String maker;
+
+    // Normal constructor
+    public Car() {
+        model = 2029;
+        maker = "Kai";
+    }
+
+    // Constructor overloading
+    public Car(int mo , String m) {
+        model = mo;
+        maker = m;
+    }
+
+    // Setter methods
+     public void set_model(int mo) {
+        model = mo;
+     }
+    public void set_maker(String m) {
+        maker = m;
+    }
+
+    // Getter methods
+    public int get_model(int mo) {
+        model = mo;
+        return model;
+    }
+    public String get_maker(String m) {
+        maker = m;
+        return maker;
+    }
 
 }
