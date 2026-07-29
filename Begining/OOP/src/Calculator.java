f58ff1ccd51720b37ceb6ac2ef5e20fd88268725
Amged Moaz <amgedmoaz7@gmail.com>
2026-07-29 21:27:52 +0300

    Add utility class

---
+++ Begining/OOP/src/Calculator.java
@@ -0,0 +1,23 @@
+ // Write a utility class to calc some arithmetic operations
+
+public class Calculator {
+
+    // Static methods
+    public static double add(double number_1 , double number_2) {
+        return (number_1 + number_2);
+    }
+    public static double subtraction(double number_1 , double number_2) {
+        if(number_1 >= number_2){
+            return (number_1 - number_2);
+        }else {
+            return (number_2 - number_1);
+        }
+    }
+    public static double multiply(double number_1, double number_2) {
+        return (number_1 * number_2);
+    }
+    public static double division(double number_1, double number_2) {
+        return (number_1 / number_2);
+    }
+
+}
