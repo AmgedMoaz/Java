e959ed27ab9cb0b7b79ba57a196591ee79f48ea1
Amged Moaz <amgedmoaz7@gmail.com>
2026-07-30 16:46:55 +0300

    Add and fill hall class

---
+++ Begining/OOP/src/Hall.java
@@ -0,0 +1,49 @@
+ // Write a class to show meaning of returning an object
+
+public class Hall {
+
+    // fields
+    private int Length;
+    private int Width;
+
+    // Normal constructor
+    public Hall() {}
+
+    // Overloading constructor
+    public Hall(int l , int w){
+        Length = l;
+        Width = w;
+    }
+
+    // Shape 1
+    public Hall add(Hall ob1 , Hall ob2) {
+        Hall result = new Hall();
+        result.Length = ob1.Length + ob2.Length;
+        result.Width = ob1.Width + ob2.Width;
+        return result;
+    }
+
+    // Shape 2
+    public Hall add(Hall ob) {
+        Hall result = new Hall();
+        result.Length = this.Length + ob.Length;
+        result.Width = this.Width + ob.Width;
+        return result;
+    }
+
+    // Shape 3
+    public Hall Add(Hall ob) {
+        ob.Length = this.Length + ob.Length;
+        ob.Width = this.Width + ob.Width;
+        return ob;
+    }
+
+    // Getter
+    public int get_Length() {
+        return Length;
+    }
+    public int get_Width() {
+        return Width;
+    }
+
+}
