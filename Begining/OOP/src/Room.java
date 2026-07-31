c8116bec9ea1c3fe39b9fb0522c766c8973936d7
Amged Moaz <amgedmoaz7@gmail.com>
2026-07-31 16:20:28 +0300

    Add a comment in class Room about isEqual method

--- Begining/OOP/src/Room.java
+++ Begining/OOP/src/Room.java
@@ -17,6 +17,7 @@ public class Room {
         ob.Length = 10;
         ob.Width = 7;
      }
+     // Add isEqual method to compare between objects
      public boolean isEqual(Room ob) {
         if(this.Width == ob.Width && this.Length == ob.Length) {
             return true;
