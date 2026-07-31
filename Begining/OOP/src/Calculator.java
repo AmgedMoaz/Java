e5bd130ab143e5ce134a26029f53bbf41e07ef8d
Amged Moaz <amgedmoaz7@gmail.com>
2026-07-31 16:59:10 +0300

    Add comments to show the purpose of each method in class Calculator

--- Begining/OOP/src/Calculator.java
+++ Begining/OOP/src/Calculator.java
@@ -3,9 +3,12 @@
 public class Calculator {
 
     // Static methods
+    // Add method
     public static double add(double number_1 , double number_2) {
         return (number_1 + number_2);
     }
+
+    // Subtraction method
     public static double subtraction(double number_1 , double number_2) {
         if(number_1 >= number_2){
             return (number_1 - number_2);
@@ -13,12 +16,18 @@ public class Calculator {
             return (number_2 - number_1);
         }
     }
+
+    // Multiply method
     public static double multiply(double number_1, double number_2) {
         return (number_1 * number_2);
     }
+
+    // Division method
     public static double division(double number_1, double number_2) {
         return (number_1 / number_2);
     }
+
+    // Modlus method
     public static double modlus(double number_1, double number_2) {
         return (number_1 % number_2);
     }
