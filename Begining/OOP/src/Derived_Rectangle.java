8ab2a901f49a0507325118e6936e2870916ec4d3
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-02 20:36:27 +0300

    Add and fill the class Derived_Rectangle

---
+++ Begining/OOP/src/Derived_Rectangle.java
@@ -0,0 +1,10 @@
+ // Write a class to use a protected members that derived from super class
+
+public class Derived_Rectangle extends Shape {
+
+    // Getter
+    public double getArea() {
+        return (Width * Length);
+    }
+
+}
