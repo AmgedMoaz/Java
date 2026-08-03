8d9dcd96e4f45d1bb955f439820cd02cf28f10f0
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-03 15:58:34 +0300

    Add and fill an interface

---
+++ Begining/OOP/src/Displayable.java
@@ -0,0 +1,8 @@
+ // Write an interface that contains the method that must be implemented
+
+public interface Displayable {
+
+    // Method that must be implemented in implementation classes
+    public void display();
+
+}
