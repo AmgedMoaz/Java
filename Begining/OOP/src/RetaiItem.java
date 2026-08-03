a5a7faed83f53306e50ef078133042f645d387e3
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-03 15:55:45 +0300

    Edit the method comment

--- Begining/OOP/src/RetaiItem.java
+++ Begining/OOP/src/RetaiItem.java
@@ -5,7 +5,7 @@ public interface RetaiItem {
     // fields
     String Store_name = "Delta";
 
-    // Method that be implemented im implementation classes
+    // Method that must be implemented im implementation classes
     public double get_itemPrice();
 
     //     مينفعش انشئ منه instant (object) في ال main او غيره زيه زي ال abstract class
