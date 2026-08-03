a09285ee2bb2f467188e695c552abc72a0caa4d0
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-03 18:40:09 +0300

    Add and fill class that contain enums

---
+++ Begining/OOP/src/RegisterForm.java
@@ -0,0 +1,24 @@
+ // Write a class and show meaning Definition ,Declaration and Assignment of enum
+
+// Definition enums
+enum Gender {Male,Female};
+enum Course {Database,AI,Web,Mobile};
+enum Semester {Summer,Winter,Spring,Autumn};
+
+public class RegisterForm {
+
+    // Foe;ds
+    private String student_name;
+    private Gender student_gender;
+    private Course student_course;
+    private Semester student_semester;
+
+    // Primary Constructor
+    public RegisterForm() {
+        student_name = "No name";
+        student_gender = Gender.Male;
+        student_course = Course.Web;
+        student_semester = Semester.Autumn;
+    }
+
+}
