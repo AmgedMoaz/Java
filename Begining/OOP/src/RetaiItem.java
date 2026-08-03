0baaea9b13a0f051ac0f2692bcb637f4f7d87653
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-03 15:10:00 +0300

    Add and fill a interface , final or static field and method to be implemented in implementation classes

---
+++ Begining/OOP/src/RetaiItem.java
@@ -0,0 +1,12 @@
+ // Write a interface and a header method that implement in implement classes
+
+public interface RetaiItem {
+
+    // fields
+    String Store_name = "Delta";
+
+    // Method that be implemented im implementation classes
+    public double get_itemPrice();
+
+    //     مينفعش انشئ منه instant (object) في ال main او غيره زيه زي ال abstract class
+}
