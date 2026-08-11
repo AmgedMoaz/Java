79082269f8e5bbfa547ed8101feb40569ea29ca0
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-11 14:00:18 +0300

    Main class for implementing many classes

---
+++ Begining/Projects/src/Main.java
@@ -0,0 +1,12 @@
+// Main class for implementing many classes
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+
+
+
+    }
+}
