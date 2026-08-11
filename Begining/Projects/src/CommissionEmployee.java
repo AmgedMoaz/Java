7da7c8552eaf7eab6529f0f76bda9a567bd4051f
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-11 15:28:06 +0300

    Add and fill derived class that implements the methods of both interface and abstract class

---
+++ Begining/Projects/src/CommissionEmployee.java
@@ -0,0 +1,32 @@
+// Write a derived class from super class and interface
+
+public class CommissionEmployee extends Employee implements Displayable {
+
+    // Fields
+    private double Gross_Sales;
+    private double Commission_rate;
+
+    // Overloading constructor
+    public CommissionEmployee(String name , String address , int SSN , double Gross_Sales , double Commission_rate) {
+        super(name,address,SSN);
+        this.Gross_Sales = Gross_Sales;
+        this.Commission_rate = Commission_rate;
+    }
+
+    // Implementing methods
+    @Override
+    public double Earning() {
+        return (Gross_Sales * Commission_rate);
+    }
+    @Override
+    public void display_All_details() {
+        System.out.println("Company Name : " + companyName + "\nFax : " + fax);
+        System.out.println("Gross Sales : " + Gross_Sales);
+        System.out.println("Commission rate : " + Commission_rate);
+    }
+    @Override
+    public void display_Earning() {
+        System.out.println( Gross_Sales * (Commission_rate/100) );
+    }
+
+}
