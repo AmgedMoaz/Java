b6d42bb678e4eb3dc82248b76f753ef7260a6e79
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-11 14:52:23 +0300

    Add and fill interface to make its methods implemented in many classes

---
+++ Begining/Projects/src/Displayable.java
@@ -0,0 +1,13 @@
+// Write an interface to be implemented in many classes
+
+public interface Displayable {
+
+    // Final and static Fields must be assigned
+    String companyName = "ABC association";
+    String fax = "1234567890";
+
+    // Methods must be implemented
+    public void display_All_details();
+    public void display_Earning();
+
+}
