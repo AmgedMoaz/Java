b3f469cb4c426b353934c99bf6a6012280e4c429
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-11 16:06:16 +0300

    Add and fill derived class that implements the methods of both interface and abstract class

---
+++ Begining/Projects/src/SalariedEmployee.java
@@ -0,0 +1,43 @@
+// Write a derived class from super class and interface
+
+public class SalariedEmployee extends Employee implements Displayable {
+
+    // Fields
+    private double salary;
+    private double bonus;
+    private double deduction;
+
+    // Overloading constructor
+    public SalariedEmployee(String name , String address , int SSN) {
+        super(name,address,SSN);
+    }
+
+    // Setters
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+    public void setDeduction(double deduction) {
+        this.deduction = deduction;
+    }
+
+    // Implementing methods
+    @Override
+    public double Earning() {
+        return (salary + bonus - deduction);
+    }
+    @Override
+    public void display_All_details() {
+        System.out.println("Company Name : " + companyName + "\nFax : " + fax);
+        System.out.println("Salary : " + salary);
+        System.out.println("Bonus : " + bonus);
+        System.out.println("Deduction : " + deduction );
+    }
+    @Override
+    public void display_Earning() {
+        System.out.println(salary+bonus-deduction);
+    }
+
+}
